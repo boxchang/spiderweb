79b39d799e6640f1290f8534211583466cc232e2
Box Chang <forkome@gmail.com>
2024-10-16 09:51:15 +0700

    UPDATE

--- factory/factory.py
+++ factory/factory.py
@@ -1,4 +1,5 @@
 from factory.factory_equipment import CountingDeviceMonitor, AOIDeviceMonitor, ScadaPLCMonitor
+from factory.key_device import KeyDeviceMonitor
 from factory.mes_data_status import MesDataStatusMonitor
 # from factory.key_device import KeyDeviceMonitor
 from factory.wecom import WecomMonitor
@@ -17,5 +18,7 @@ class MonitorFactory:
             return MesDataStatusMonitor(device_type)
         elif device_type == 'WECOM':
             return WecomMonitor(device_type)
+        elif device_type == 'KEY_DEVICE':
+            return KeyDeviceMonitor(device_type)
         else:
             raise ValueError(f"Unknown device type: {device_type}")
