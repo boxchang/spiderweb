1407655c45e1cc2b24213dcce4f1140f8522cf38
Box Chang <forkome@gmail.com>
2024-10-26 10:18:08 +0700

    UPDATE

--- factory/wecom.py
+++ factory/wecom.py
@@ -24,25 +24,22 @@ class WecomMonitor(Monitor):
 
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         data = {
-            "msgtype": "text",
-            "text": {
+            "msgtype": "markdown",
+            "markdown": {
                 "content": '',
                 # "mentioned_list": ["@all"],
             }
         }
-        data["text"]["content"] = msg
+        data["markdown"]["content"] = msg
         r = requests.post(url, headers=headers, json=data)
         return r.json()
 
     def send_msg(self, vnedc_db):
         comment = ""
-        msg = """
-        [Issue #{row_id}]
-        Type: {device_type}
-        Device: {device_name}
-        {comment}
-        """
-
+        msg = """[Issue #{row_id}]
+Type: **{device_type}**
+Device: **{device_name}**
+{comment}"""
         sql = f"""
                 SELECT smdlog.id, smdlog.func_name, smdlog.comment, smdlist.device_name,
                 smdlog.status_code_id error_status, smdlist.status_id current_status, smdlog.notice_flag, smdlog.recover_msg,
@@ -53,14 +50,13 @@ class WecomMonitor(Monitor):
                 FROM [VNEDC].[dbo].[spiderweb_monitor_device_log] smdlog
                 JOIN [VNEDC].[dbo].[spiderweb_monitor_device_list] smdlist
                 on smdlog.device_id = smdlist.id
-                where smdlog.recover_msg is null                   
+                --where smdlog.recover_msg is null                   
                 """
         rows = vnedc_db.select_sql_dict(sql)
         if len(rows) > 0:
             for row in rows:
-
+                comment = row['comment']
                 if int(row['notice_flag']) == 0:
-                    comment = row['comment']
                     Log.update_log_flag(vnedc_db, row['id'])
                 elif int(row['notice_flag']) == 1:
                     if str(row['error_status'])[0] == 'E'  and str(row['current_status']) == 'S':
@@ -68,7 +64,7 @@ class WecomMonitor(Monitor):
                     elif str(row['error_status'])[0] == 'E' and str(row['current_status']) == 'E':
                         comment = f"now change to {row['comment']}"
                     Log.update_msg_flag(vnedc_db, row['id'])
-                msg.format(row_id=row['id'], device_type=row['func_name'], device_name=row['device_name'], comment=comment)
+                msg = msg.format(row_id=row['id'], device_type=row['func_name'], device_name=row['device_name'], comment=comment)
                 self.send_wecom(msg)
 
 
